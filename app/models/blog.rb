6f506dcb2e00ea7f75dbc1bc618bca4c9402ee3e
Miguel Saldana <mig.saldana@gmail.com>
2017-10-08 20:17:45 -0500

    Integreated database relationshipts between topics and blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,11 +6,14 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, :use => [:slugged, :history]
 
-  validates_presence_of :title, :body
-
-
   def should_generate_new_friendly_id?
     slug.blank? || title_changed? || super
   end
+
+  validates_presence_of :title, :body
+  belongs_to :topic
+
+
+
 
 end
